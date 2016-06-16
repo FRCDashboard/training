1ad7ff8171922dd9085d3dd9ac58ddc013c09dec
ErikBoesen <me@erikboesen.com>
2016-06-15 21:04:26 -0400

    Init, no tasks yet but groundwork laid

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# training
-Training exercise for learning FRC Dashboard.
+# FRC Dashboard Training
+This repository serves as a training exercise for students and mentors wishing to learn to use the [FRC Dashboard](https://github.com/FRCDashboard/FRCDashboard) FRC user interface development library.
+
+## Prerequisites for this trainer
+FRC Dashboard uses JavaScript, HTML, and CSS code. It is HIGHLY recommended that you gain at least some knowledge of all three of these languages before attempting this training or building on to FRC Dashboard. [Codecademy](https://codecademy.com)'s courses "JavaScript" and "HTML & CSS" are a quick and easy way to get up and running with these languages, and thus FRC Dashboard and other web-based software.
+
+To start the server, you'll also need very basic knowledge of using the Terminal (or cmd, if you're on Windows). We recommend checking out Codecademy's course on the Terminal if you need help with this.
+
+Note: This software also uses a web server which is written in Python. You probably won't need to do modify this, so knowledge of Python isn't really necessary. However, if you're interested (which is great), Codecademy has a course for that too.
+
+## Dependencies:
+* Python 3
+* `pynetworktables2js`:
+
+    pip3 install pynetworktables2js
+
+(If you don't have administrator privileges, you can put `--user` at the end of that command to install the package for only your account.)
+
+## Starting the server
+Use the `cd` command in Terminal (Linux/Mac) or cmd (Windows) to navigate to this directory. Then execute:
+
+    python3 dashboardServer.py
+
+This will start the server, hosting your code at http://localhost:8888. Open that page to view your dashboard or to use it in competition.
+
+## Tasks
+1. TODO
---
+++ ui.js
@@ -0,0 +1,50 @@
+// This object contains a list of all the controls on the dashboard. If you add a new interactive element, you'll need to put it in this object.
+var ui = {
+	light: document.getElementById('light'),
+    winch: document.getElementById('winch')
+};
+
+// Sets function to be called on NetworkTables connect. Commented out because it's usually not necessary.
+// NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
+
+// Sets function to be called when robot dis/connects. Commented out because it's not an essential thing to learn and you usually won't need to touch it.
+// NetworkTables.addRobotConnectionListener(onRobotConnection, true);
+
+// Sets function to be called when any NetworkTables key/value changes
+NetworkTables.addGlobalListener(onValueChanged, true);
+
+/*
+    @param key: The name of the variable.
+    @param value: The value of the variable.
+    @param isNew: Boolean, true if the value has not yet been passed to this instance of FRC Dashboard.
+*/
+function onValueChanged(key, value, isNew) {
+	// Sometimes, NetworkTables will pass booleans as strings. This corrects for that. You probably shouldn't have to touch this code.
+	if (value == 'true') {
+		value = true;
+	} else if (value == 'false') {
+		value = false;
+	}
+
+	// This switch statement chooses which UI element to update when a NetworkTables variable changes.
+	// So, if the robot changes the value of a NetworkTables variable, the state of a control will change.
+	switch (key) {
+		case '/SmartDashboard/light':
+			// Set the checkedness of this control element.
+			ui.light.checked = value;
+	}
+	// In the real dashboard, there will be more code here to manage the Tuning section of the UI.
+	// That code is not included in this example because it's very confusing and you usually don't need to modify it anyway.
+}
+
+// The folllowing functions are called when the user interacts with control elements.
+
+ui.light.onclick = function() {
+	// Set NetworkTables value to the new checked state of the checkbox.
+	NetworkTables.setValue('/SmartDashboard/light', ui.light.checked);
+};
+
+ui.winch.onclick = function() {
+    // Set the winch's openness to the opposite of what it currently is.
+    NetworkTables.setValue('/SmartDashboard/winch', !NetworkTables.getValue('/SmartDashboard/winch'));
+};
